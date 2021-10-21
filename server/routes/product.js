1fe8c389f07ce3c8929ef58cf7f9c637b81e7bdf
Jinwoong <tjwlsdnd12@gmail.com>
2021-10-21 18:56:34 +0900

    2021-10-21 리액트노드쇼핑몰강의 업로드

---
+++ server/routes/product.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const multer = require('multer');
+
+//=================================
+//             Product
+//=================================
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+router.post("/image", (req, res) => {
+  // 가져온 이미지를 저장을 해주며 된다.
+  upload(req, res, err => {
+      if(err) {
+          return res.json({success: false, err})
+      }
+      return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename})
+  })
+});
+
+module.exports = router;
